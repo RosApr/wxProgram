394bb417682af2eb27677d4a9211edfdb1028bae
杨小刚 <nulijiuhao712@126.com>
2018-09-25 22:25:56 +0800

    发布选择效果补全

--- src/pages/step1/index.vue
+++ src/pages/step1/index.vue
@@ -1,8 +1,17 @@
 <template>
     <div class="step1-container">
-        <div class="grid" @click="check(typeConfig['sell'])" :class="{'active': active == typeConfig['sell']}"></div>
-        <div class="grid" @click="check(typeConfig['buy'])" :class="{'active': active == typeConfig['buy']}"></div>
-        <div class="grid" @click="check(typeConfig['logistics'])" :class="{'active': active == typeConfig['logistics']}"></div>
+        <div class="grid">
+            <div class="img-container" @click="check(typeConfig['sell'])" :class="{'active': active == typeConfig['sell']}"></div>
+            <p>厨具</p>
+        </div>
+        <div class="grid">
+            <div class="img-container" @click="check(typeConfig['buy'])" :class="{'active': active == typeConfig['buy']}"></div>
+            <p>求购</p>
+        </div>
+        <div class="grid">
+            <div class="img-container" @click="check(typeConfig['logistics'])" :class="{'active': active == typeConfig['logistics']}"></div>
+            <p>物流</p>
+        </div>
     </div>
 </template>
 <script>
@@ -11,7 +20,7 @@
     export default {
         data() {
             return {
-                active: INDEX_PAGE_LIST_TYPE['sell'],
+                active: "",
                 typeConfig: INDEX_PAGE_LIST_TYPE
             }
         },
@@ -43,25 +52,41 @@
         .grid {
             width: 100%;
             height: 33.333333vh;
+            .img-container {
+                height: 20vh;
+                &.active + p {
+                    color: #48cb53;
+                }
+            }
+            p {
+                text-align: center;
+                color: #878787;
+            }
             &:nth-child(1) {
-                background: url("../../../static/images/sell.png") center center no-repeat;
-                background-size: 20%;
-                &.active {
-                    background-image: url("../../../static/images/sell.png");
+                .img-container {
+                    background: url("../../../static/images/sell.png") center center no-repeat;
+                    background-size: 20%;
+                    &.active {
+                        background-image: url("../../../static/images/sell_a.png");
+                    }
                 }
             }
             &:nth-child(2) {
-                background: url("../../../static/images/buy.png") center center no-repeat;
-                background-size: 20%;
-                &.active {
-                    background-image: url("../../../static/images/buy.png");
+                .img-container {
+                    background: url("../../../static/images/buy.png") center center no-repeat;
+                    background-size: 20%;
+                    &.active {
+                        background-image: url("../../../static/images/buy_a.png");
+                    }
                 }
             }
             &:nth-child(3) {
-                background: url("../../../static/images/log.png") center center no-repeat;
-                background-size: 20%;
-                &.active {
-                    background-image: url("../../../static/images/log.png");
+                .img-container {
+                    background: url("../../../static/images/log.png") center center no-repeat;
+                    background-size: 20%;
+                    &.active {
+                        background-image: url("../../../static/images/log_a.png");
+                    }
                 }
             }
         }
