76ca29ed2022601e686a3ef2466c6de7eb24bfcc
杨小刚 <nulijiuhao712@126.com>
2018-12-05 10:06:47 +0800

    接口地址变更

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import wx from './wx'
 import Fly from 'flyio'
 import WXP from 'minapp-api-promise'
-const baseUrl = "https://m.91lzht.com/index.php/"
+const baseUrl = "https://m.91lzht.com/index.php/api"
 import { TOKEN, OPEN_ID, REGION, isInStorageAndReturn } from "@/utils/common"
 const request = new Fly()
 request.config.baseURL = baseUrl
